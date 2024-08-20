2fe6cf737dd9042abf9898c08c04bc0f2e93322b
hrIthika21 <kskhrithika@gmail.com>
2024-08-20 17:22:48 +0530

    Update MyProject.jsx

--- src/MyProject.jsx
+++ src/MyProject.jsx
@@ -9,28 +9,28 @@ const MyProject= () => {
                 <div className="project-card-1">
                     <h1 className="project-title">MUSIC WEBSITE</h1>
                     <div className="project-card-1-part-2">
-                        <a href="https://github.com/hrIthika21/hrIthika21.github.io.git"><img src="src/assets/Screenshot 2024-08-10 at 12.06.44 PM.jpg" alt="project-1" className="project-1" /></a>
+                        <a href="https://github.com/hrIthika21/hrIthika21.github.io.git"><img src="src/assets/Screenshot 2024-08-10 at 12.06.44 PM.JPG" alt="project-1" className="project-1" /></a>
                         <p >An music website created to appeal the viewers but it does not play any songs.Click the image to get source code</p>
                     </div>
                 </div>
                 <div className="project-card-2">
                     <h1 className="project-title">TIC-TAC-TOE</h1>
                     <div className="project-card-2-part-2">
-                        <a href="https://github.com/hrIthika21/tic-tac-toe-game.git"><img src="src/assets/Screenshot 2024-08-10 at 12.10.05 PM.jpg" alt="project-2" className="project-2" /></a>
+                        <a href="https://github.com/hrIthika21/tic-tac-toe-game.git"><img src="src/assets/Screenshot 2024-08-10 at 12.10.05 PM.JPG" alt="project-2" className="project-2" /></a>
                         <p>Play TIC-TAC-TOE with your friends and track your winning streak.Click the image to get source code</p>
                     </div>
                 </div>
                 <div className="project-card-3">
                     <h1 className="project-title">EXPENSE TRACKER</h1>
                     <div className="project-card-3-part-2">
-                        <a href="https://github.com/hrIthika21/Expense_tracker.git"><img src="src/assets/Screenshot 2024-08-10 at 12.11.43 PM.jpg" alt="project-3" className="project-3"/></a>
+                        <a href="https://github.com/hrIthika21/Expense_tracker.git"><img src="src/assets/Screenshot 2024-08-10 at 12.11.43 PM.JPG" alt="project-3" className="project-3"/></a>
                         <p> Add, delete, modify and calculate the expense using this website.Click the image to get source code</p>
                     </div>
                 </div>
                 <div className="project-card-4">
                     <h1 className="project-title">WEATHER WEBSITE</h1>
                     <div className="project-card-4-part-2">
-                        <a href="https://github.com/hrIthika21/Weather_app.git"><img src="src/assets/Weather_website.jpg" alt="project-4" className="project-4" /></a>
+                        <a href="https://github.com/hrIthika21/Weather_app.git"><img src="src/assets/Weather_website.JPG" alt="project-4" className="project-4" /></a>
                         <p>Displays current weather condition,forecast for next 24 hours and 5 days.Click the image to get source code</p>
                     </div>
                 </div>
